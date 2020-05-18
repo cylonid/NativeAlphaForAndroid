37d03abe583ed3e305b536f971f709849493054b
Hannes Unterweger <h.unterweger@student.tugraz.at>
2020-05-18 17:50:04 +0200

    Basic dialogue example implemented

--- app/src/main/java/com/cylonid/nativefier/MainActivity.java
+++ app/src/main/java/com/cylonid/nativefier/MainActivity.java
@@ -1,11 +1,14 @@
 package com.cylonid.nativefier;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -20,6 +23,7 @@ import com.google.android.material.snackbar.Snackbar;
 import static android.widget.LinearLayout.HORIZONTAL;
 
 public class MainActivity extends AppCompatActivity {
+    AlertDialog.Builder dialogue_add_website;
 
 
     @Override
@@ -38,12 +42,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+
+
+
         LinearLayout mainScreen = (LinearLayout) findViewById(R.id.mainScreen);
         WebsiteDataManager.getInstance().initContext(this);
         WebsiteDataManager.getInstance().initDummyData();
         for (WebsiteData d : WebsiteDataManager.getInstance().getWebsites())
             addRow(mainScreen, d);
-
+        dialogue_add_website = new AlertDialog.Builder(this);
+        configAddWebsiteDialogue();
+        dialogue_add_website.show();
     }
 
     @Override
@@ -124,6 +136,30 @@ public class MainActivity extends AppCompatActivity {
 
         mainScreen.addView(ll_row);
     }
+
+    private void configAddWebsiteDialogue() {
+        // Get the layout inflater
+        LayoutInflater inflater = getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        dialogue_add_website.setView(inflater.inflate(R.layout.add_website_dialogue, null));
+        dialogue_add_website.setTitle("Add new website");
+        // Add the buttons
+        dialogue_add_website.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        dialogue_add_website.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        AlertDialog dialog = dialogue_add_website.create();
+    }
+
+
 
 }
 
