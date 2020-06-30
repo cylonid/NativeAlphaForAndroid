3a3367270baeeb3e5fa60fefa5cc92814e8d1bd1
Hannes Unterweger <h.unterweger@student.tugraz.at>
2020-06-30 15:14:29 +0200

    Splash screen added

--- app/src/main/java/com/cylonid/nativefier/MainActivity.java
+++ app/src/main/java/com/cylonid/nativefier/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainScreen = (LinearLayout) findViewById(R.id.mainScreen);
